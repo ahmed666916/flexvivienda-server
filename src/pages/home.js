91987cfd9465911121c9bae2a64ff051c58bacd5
Ahmed Adnan <ahmed666916@gmail.com>
2025-05-13 18:40:25 +0500

    removed redundant searchbar from home screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
-import SearchBar from '../components/SearchBar/SearchBar';
 import PropertyList from '../components/PropertyList/PropertyList';
 import Testimonials from '../components/Testimonial/Testimonial';
 
@@ -10,7 +9,7 @@ const Home = () => {
 
         <>
         <Hero />
-        <SearchBar />
+        
         <PropertyList />
         <Testimonials />
 
