c5d808938e88e197d78d11cff8b774edabe436f0
Ahmed Adnan <ahmed666916@gmail.com>
2025-05-14 23:06:47 +0500

    added experience section to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero/Hero';
 import PropertyList from '../components/PropertyList/PropertyList';
 import Testimonials from '../components/Testimonial/Testimonial';
+import ExperienceSection from './components/ExperienceSection/ExperienceSection';
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
         
         <PropertyList />
         <Testimonials />
+        <Experience />
 
         </>
 
