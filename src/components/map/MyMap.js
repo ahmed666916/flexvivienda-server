25b9d3d4dac71ee392677be1cd905aa9109246ac
ahmed666916 <ahmed666916@gmail.com>
2025-07-03 17:49:08 +0500

    added and fixed heading at the top of the map

--- src/components/map/MyMap.js
+++ src/components/map/MyMap.js
@@ -1,10 +1,11 @@
+// src/components/MyMap.js
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import './MapStyles.css';
 
-// Fix default marker icon issue in React
+// Fix Leaflet default icon issue
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -14,19 +15,22 @@ L.Icon.Default.mergeOptions({
 
 const MyMap = () => {
   return (
-    <div className="map-wrapper">
-      <MapContainer center={[39.0, 35.0]} zoom={8} style={{ height: '500px', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-        />
-        <Marker position={[39.0, 35.0]}>
-          <Popup>
-            A pretty popup.<br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <section className="map-section">
+      <h2 className="map-heading">Live Map</h2>
+      <div className="map-wrapper">
+        <MapContainer center={[39.0, 35.0]} zoom={8} style={{ height: '500px', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; OpenStreetMap contributors &copy; <a href="https://carto.com/">CARTO</a>'
+          />
+          <Marker position={[39.0, 35.0]}>
+            <Popup>
+              A pretty popup.<br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </section>
   );
 };
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -11,6 +11,10 @@ import TrustSignals from '../components/ownerListing/TrustSignals';
 import FutureOfLivingSection from '../components/FutureOfLivingSection/FutureOfLivingSection';
 import BetterHomeSlider from '../components/BetterHomeSlider/BetterHomeSlider';
 import './home.css';
+import MyClusterMap from '../components/map/MyClusterMap';
+
+
+
 
 
 
@@ -25,6 +29,12 @@ const Home = () => {
         <>
         <Hero heading={t('heroMessage')} subheading={t('heroSubtitle')}/>
         {/* <SearchBar /> */}
+      {/* Map Section */}
+      <div className="map-heading-container">
+      <h2 className="map-heading">Live Map of Available Properties</h2>
+      </div>
+      
+
        <div className="section-block featured-section">
       <PropertyList title="Featured Listings" tabs="0" maps="1" tags="1" short="1"/>
       </div>
