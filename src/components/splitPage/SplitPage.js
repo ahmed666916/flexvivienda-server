c8f98af3e26bc778ee9ebecf5fc2f33898bdfec7
ahmed666916 <ahmed666916@gmail.com>
2025-07-06 22:08:12 +0500

    added images to split page

--- src/components/splitPage/SplitPage.js
+++ src/components/splitPage/SplitPage.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import './SplitPage.css';
 import { Link } from 'react-router-dom';
-import { FaHome, FaChartLine } from 'react-icons/fa';
+import hostBg from '../../assets/host-bg-resized.png';
+import investBg from '../../assets/invest-bg-resized.png';
 
 const SplitPage = () => {
   return (
     <div className="split-container">
       <Link to="/owner_listing" className="split-link">
-        <div className="split left hoverable">
-          <div className="content">
-            <FaHome className="icon" />
-            <h1>Become a Host</h1>
-            <p>Share your space and earn income with ease.</p>
-          </div>
-        </div>
+        <div
+          className="split left hoverable"
+          style={{ backgroundImage: `url(${hostBg})` }}
+        />
       </Link>
 
       <div className="divider"></div>
 
       <Link to="/invest" className="split-link">
-        <div className="split right hoverable">
-          <div className="content">
-            <FaChartLine className="icon" />
-            <h1>Invest</h1>
-            <p>Grow your wealth by investing in properties.</p>
-          </div>
-        </div>
+        <div
+          className="split right hoverable"
+          style={{ backgroundImage: `url(${investBg})` }}
+        />
       </Link>
     </div>
   );
