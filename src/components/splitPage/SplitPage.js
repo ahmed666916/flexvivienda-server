e0054b6dcd82000b144cc301d725170966cede08
ahmed666916 <ahmed666916@gmail.com>
2025-07-06 21:09:30 +0500

    updated split page

--- src/components/splitPage/SplitPage.js
+++ src/components/splitPage/SplitPage.js
@@ -12,7 +12,6 @@ const SplitPage = () => {
             <FaHome className="icon" />
             <h1>Become a Host</h1>
             <p>Share your space and earn income with ease.</p>
-           
           </div>
         </div>
       </Link>
@@ -25,7 +24,6 @@ const SplitPage = () => {
             <FaChartLine className="icon" />
             <h1>Invest</h1>
             <p>Grow your wealth by investing in properties.</p>
-           
           </div>
         </div>
       </Link>
