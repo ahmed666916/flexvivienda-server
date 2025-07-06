25e8beb217e8b712266c65411e9a96a42f91f3e1
ahmed666916 <ahmed666916@gmail.com>
2025-07-06 23:27:29 +0500

    added marked properties in map

--- src/components/map/MyClusterMap.js
+++ src/components/map/MyClusterMap.js
@@ -1,5 +1,3 @@
-// src/components/MyClusterMap.js
-
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
@@ -7,7 +5,7 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import L from 'leaflet';
-import './MapStyles.css'; // Make sure this contains your .map-wrapper and .map-heading styles
+import './MapStyles.css';
 
 // Leaflet marker icon fix
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
@@ -26,9 +24,51 @@ const cityCoordinates = {
   'Beyoğlu, Istanbul': [41.0369, 28.9760],
   'Ortaköy, Istanbul': [41.0431, 29.0222],
   'Kadıköy, Istanbul': [40.9902, 29.0275],
+  'Bakırköy, Istanbul': [40.9719, 28.8535],
+  'Taksim, Istanbul': [41.0365, 28.9850],
+  'Şişli, Istanbul': [41.0605, 28.9872],
 };
 
-const MyClusterMap = ({ properties = [], onLoad }) => {
+const defaultProperties = [
+  {
+    id: 1,
+    title: 'Modern Flat near Istiklal Street',
+    location: 'Beyoğlu, Istanbul',
+    price: '€120/night',
+  },
+  {
+    id: 2,
+    title: 'Superb House in Ortaköy',
+    location: 'Ortaköy, Istanbul',
+    price: '€200/night',
+  },
+  {
+    id: 3,
+    title: 'Cozy Studio in Kadıköy',
+    location: 'Kadıköy, Istanbul',
+    price: '€90/night',
+  },
+  {
+    id: 4,
+    title: 'Sea View Apartment in Bakırköy',
+    location: 'Bakırköy, Istanbul',
+    price: '€150/night',
+  },
+  {
+    id: 5,
+    title: 'Luxury Central Flat',
+    location: 'Taksim, Istanbul',
+    price: '€250/night',
+  },
+  {
+    id: 6,
+    title: 'Stylish Retreat with Jacuzzi',
+    location: 'Şişli, Istanbul',
+    price: '€220/night',
+  },
+];
+
+const MyClusterMap = ({ properties = defaultProperties, onLoad }) => {
   const [map, setMap] = useState(null);
 
   useEffect(() => {
@@ -43,10 +83,16 @@ const MyClusterMap = ({ properties = [], onLoad }) => {
 
   return (
     <section className="map-wrapper">
-            <div style={{ height: '500px', width: '100%' }}>
+      <center>
+        <h2 className="heading">
+          <span className="heading-primary">Live</span>{' '}
+          <span className="heading-secondary">Map</span>
+        </h2>
+      </center>
+      <div style={{ height: '500px', width: '100%' }}>
         <MapContainer
           center={[41.0151, 28.9795]}
-          zoom={12}
+          zoom={11}
           style={{ height: '100%', width: '100%' }}
           whenCreated={setMap}
         >
@@ -57,11 +103,11 @@ const MyClusterMap = ({ properties = [], onLoad }) => {
 
           <MarkerClusterGroup chunkedLoading>
             {properties.map((property, index) => {
-              const baseCoords = cityCoordinates[property.location];
-              if (!baseCoords) return null;
+              const coords = cityCoordinates[property.location];
+              if (!coords) return null;
 
               const jitter = (Math.random() - 0.5) * 0.005;
-              const position = [baseCoords[0] + jitter, baseCoords[1] + jitter];
+              const position = [coords[0] + jitter, coords[1] + jitter];
 
               return (
                 <Marker
