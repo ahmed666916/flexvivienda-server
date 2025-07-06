ddc31f4c02bfbe4f56b6e8c711fabf9ec4d0b188
ahmed666916 <ahmed666916@gmail.com>
2025-07-06 22:25:48 +0500

    fixed images text on split page

--- src/components/splitPage/SplitPage.js
+++ src/components/splitPage/SplitPage.js
@@ -3,6 +3,7 @@ import './SplitPage.css';
 import { Link } from 'react-router-dom';
 import hostBg from '../../assets/host-bg-resized.png';
 import investBg from '../../assets/invest-bg-resized.png';
+import { FaHome, FaChartLine } from 'react-icons/fa';
 
 const SplitPage = () => {
   return (
@@ -11,16 +12,26 @@ const SplitPage = () => {
         <div
           className="split left hoverable"
           style={{ backgroundImage: `url(${hostBg})` }}
-        />
+        >
+          <div className="overlay-content">
+            <FaHome className="overlay-icon" />
+            <h1>Become a Host</h1>
+            <p>Share your space and earn income with ease.</p>
+          </div>
+        </div>
       </Link>
-
-      <div className="divider"></div>
 
       <Link to="/invest" className="split-link">
         <div
           className="split right hoverable"
           style={{ backgroundImage: `url(${investBg})` }}
-        />
+        >
+          <div className="overlay-content">
+            <FaChartLine className="overlay-icon" />
+            <h1>Invest</h1>
+            <p>Grow your wealth by investing in properties.</p>
+          </div>
+        </div>
       </Link>
     </div>
   );
