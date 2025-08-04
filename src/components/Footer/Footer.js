7f76fad8ea069d0eafb3219c160683c29bf2edc0
ahmed666916 <ahmed666916@gmail.com>
2025-08-04 06:02:23 +0500

    updated bg of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
-import { FaInstagram, FaLinkedin, FaFacebook, FaWhatsapp, FaEnvelope, FaPhone } from 'react-icons/fa';
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaFacebook,
+  FaWhatsapp,
+  FaEnvelope,
+  FaPhone,
+} from 'react-icons/fa';
 
 const Footer = () => (
-  <footer className="footer">
-    <div className="footer-container">
+  <footer
+    className="footer"
+    style={{
+      backgroundImage: 'url("/Images/logo-bg.png")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      position: 'relative',
+      zIndex: 0,
+    }}
+  >
+    {/* Overlay tint */}
+    <div className="footer-overlay" />
 
-      {/* Left: Logo + Mission Statement */}
+    <div className="footer-container">
+      {/* Left: Mission Statement */}
       <div className="footer-left">
-        <img src="/Images/logo.png" alt="Flex Logo" className="footer-logo" />
         <p>Empowering modern stays with smart hospitality solutions.</p>
       </div>
 
-      {/* Center: Navigation Links */}
-      <div className="footer-center">
-        <h4>Quick Links</h4>
-        <ul className="footer-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/explore">Explore</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/terms">Terms</Link></li>
-        </ul>
+      {/* Center: Logo + Overlay Text */}
+      <div className="footer-center footer-logo-block">
+        <div className="footer-logo-overlay">
+          <img src="/Images/logo.png" alt="Flex Logo" className="footer-bg-logo" />
+          <p className="footer-overlay-text">Smart | Connected | Traditional | Modern</p>
+        </div>
       </div>
 
       {/* Right: Contact Info */}
@@ -45,7 +58,6 @@ const Footer = () => (
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
         </div>
       </div>
-
     </div>
 
     <div className="footer-bottom">
